3e8956d9ab61455479574d98cc72ccf0f5ef1692
임동균 <70128434+Dong-Es@users.noreply.github.com>
2022-04-04 17:57:50 +0900

    Create 3wk.r

---
+++ university/3wk.r
@@ -0,0 +1,56 @@
+x=1
+y=2
+z=x+y
+z
+
+##R의 경우
+
+x <- 1
+y <- 2
+z <- x+y
+z
+x+y -> z
+z
+
+#두 값을 교환
+x=1
+y=2
+temp = x
+x=y
+y = temp
+x
+y
+
+x=5
+y=2
+x/y
+
+
+#숫자 뒤에 알파벳을 붙혀야 복소수이다.
+x=1+2i
+y=1-2i
+x+y
+
+str="hello world"
+str
+
+#compostite 합성하다.
+blood.type = factor(c('a','b','c','ab'))
+blood.type
+
+T
+F
+
+c(1:5)
+
+c(1,2,3,c(4:6))
+
+x=c(1,2,3)
+
+y=c()
+y=c(y,c(1:3))
+y
+
+
+seq(from=1,to=10,by=2)
+seq(1,10,by=2)
